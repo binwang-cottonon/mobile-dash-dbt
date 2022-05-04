55427caf639572f26148c7f2f939ddd1af20f85d
Bin Wang <bin.wang@cottonon.com.au>
2022-05-04 10:23:58 +1000

    rename wip

--- models/staging/stg__app_transactions_new.sql
+++ models/staging/stg__app_transactions_new.sql
@@ -6,7 +6,7 @@ select
   ,country
   ,operating_system
   ,max(event_value_in_usd) as event_value_in_usd
-from {{ ref('base__app_logs_wip') }},
+from {{ ref('base__app_logs_new') }},
      unnest (user_properties) as up
 where event_name = 'ecommerce_purchase' or event_name = 'purchase' --and up.key = 'ga_session_id'
 group by 1,2,3,4,5
