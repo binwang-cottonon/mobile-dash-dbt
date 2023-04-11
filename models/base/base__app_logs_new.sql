27c89e89cf81d7f24b6481893b659abe534564cb
bin wang <nicola.shawyer@cottonon.com.au>
2023-04-11 23:28:11 +0000

    tweak to android streamid

--- models/base/base__app_logs_new.sql
+++ models/base/base__app_logs_new.sql
@@ -9,12 +9,12 @@ with l90d_plus_intraday as (
   from {{ source('analytics_195776711', 'events_*')}}
   where _table_suffix > format_date('%Y%m%d', date_sub(current_date(), interval 3 day))
         --EDIT START 11/4 to filter to old app
-         and stream_id in ('1425059297','1425059297')
+         and stream_id in ('1425059297','1856522162')
         --EDIT END  11/4
   union all
   select * from {{ source('analytics_195776711', 'events_intraday_*')}} 
         --EDIT START 11/4 to filter to old app
-        where stream_id in ('1425059297','1425059297')
+        where stream_id in ('1425059297','1856522162')
         --EDIT END  11/4
 )
 
